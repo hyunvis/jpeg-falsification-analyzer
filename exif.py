b1c1144cbb32eece22f466d484c1f70c710ff4b7
신현우(SHIN HYUNWOO) <bob.hyunwooshin@gmail.com>
2018-04-16 14:20:13 +0900

    add exif datetime
    exif datetime

--- exif.py
+++ exif.py
@@ -1,18 +1,29 @@
-import sys
+import os, sys
 import exifread
 
 print('Input file')
 f = sys.argv[1]
 handle = open(f, 'rb')
+tags = exifread.process_file(handle)
+
+DateTime = tags["Image DateTime"]
+DateTimeOriginal = tags["EXIF DateTimeOriginal"]
+DateTimeDigitized = tags["EXIF DateTimeDigitized"]
+
+print(DateTime)
+print(DateTimeOriginal)
+print(DateTimeOriginal)
 
 print(sys.argv[1])
+
 handle.seek(0)
 checker = handle.read(100)
 
 if ord(checker[0]) == 0xFF and ord(checker[1]) == 0xD8 : 
 	print('JPEG Checked')
+	#print(tags)
+
+
 else:
 	print('Not JPEG')
 
-tags = exifread.process_file(handle)
-print(tags)
