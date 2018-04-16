146e5de6241ea988cc2108707dc6591f2acfe9da
신현우(SHIN HYUNWOO) <bob.hyunwooshin@gmail.com>
2018-04-16 16:08:27 +0900

    add Time/Exif calculation

--- exif.py
+++ exif.py
@@ -1,29 +1,55 @@
 import os, sys
 import exifread
+import datetime
 
 print('Input file')
 f = sys.argv[1]
 handle = open(f, 'rb')
-tags = exifread.process_file(handle)
 
+tags = exifread.process_file(handle)
 DateTime = tags["Image DateTime"]
 DateTimeOriginal = tags["EXIF DateTimeOriginal"]
 DateTimeDigitized = tags["EXIF DateTimeDigitized"]
 
+print('EXIF DATA')
+print('DateTime')
 print(DateTime)
+print('DateTimeOriginal')
 print(DateTimeOriginal)
-print(DateTimeOriginal)
+print('DateTimeDigitized')
+print(DateTimeDigitized)
 
-print(sys.argv[1])
+ctime = datetime.datetime.fromtimestamp(os.path.getctime(f)) #create time
+mtime = datetime.datetime.fromtimestamp(os.path.getmtime(f)) #modify time
+atime = datetime.datetime.fromtimestamp(os.path.getatime(f)) #access time
+
+atimea = atime.strftime('%Y:%m:%d %H:%M:%S')
+mtimea = mtime.strftime('%Y:%m:%d %H:%M:%S')
+ctimea = ctime.strftime('%Y:%m:%d %H:%M:%S')
+print('\n')
+print('SYSTEM DATA')
+print('ctime')
+print(ctimea)
+print('mtime')
+print(mtimea)
+print('atime')
+print(atimea)
+
+print('test')
+print(DateTime)
+print(mtimea)
+
+if (DateTime) != (mtimea) :
+	print('File Falsification Detected!!!!')
+else:
+	print('Good as well')
+
 
 handle.seek(0)
 checker = handle.read(100)
 
 if ord(checker[0]) == 0xFF and ord(checker[1]) == 0xD8 : 
 	print('JPEG Checked')
-	#print(tags)
-
-
 else:
 	print('Not JPEG')
 
