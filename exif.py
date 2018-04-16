d587fc717e67760620e9c5d2ebf9178d218a3f27
신현우(SHIN HYUNWOO) <bob.hyunwooshin@gmail.com>
2018-04-16 11:53:11 +0900

    add input file
    added input file, exifread

--- exif.py
+++ exif.py
@@ -1,12 +1,18 @@
-handle = open('/Users/hyunvis/Desktop/IMG.jpg')
+import sys
+import exifread
+
+print('Input file')
+f = sys.argv[1]
+handle = open(f, 'rb')
+
+print(sys.argv[1])
 handle.seek(0)
 checker = handle.read(100)
-	
 
 if ord(checker[0]) == 0xFF and ord(checker[1]) == 0xD8 : 
 	print('JPEG Checked')
-
-
-
 else:
 	print('Not JPEG')
+
+tags = exifread.process_file(handle)
+print(tags)
